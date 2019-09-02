9b421d253877711d7c8db5aad058be355b0f047b
wxlMite <wxlmite@wxlMitedeMacBook-Air.local>
2019-09-02 20:35:22 +0800

    首页右边导航栏和头部导航栏完善

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { Observable } from 'rxjs';
+import { HttpService } from '../services/http-service';
+
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.less']
 })
-export class Layout{
+
+export class Layout {
   title = 'layout';
+  private url = '/api/user/loginOut';
+  private isShowMenu = false;
+  private isShowLeftChild = false;
+  constructor(private http: HttpService) {
+
+  }
+
+  loginOut() {
+    this.http.get(this.url, {}, function (res) {
+      console.log(res);
+    });
+  }
+  isShowMenuF() {
+    if (!this.isShowMenu) {
+      this.isShowMenu = true;
+    } else {
+      this.isShowMenu = false;
+    }
+
+  }
+  isShowLeftChildF() {
+    if (!this.isShowLeftChild) {
+      this.isShowLeftChild = true;
+    } else {
+      this.isShowLeftChild = false;
+    }
+  }
+
 }
